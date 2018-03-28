cd3ecd17a56f069e41d2b660ad59942c72c39c48
sober_e1@qq.com <sober_e1@qq.com>
2018-03-28 16:57:24 +0800

    二级分类商品

--- pages/index/index.js
+++ pages/index/index.js
@@ -45,6 +45,12 @@ Page({
     this.setData({
       isLoading: 1,
       num:num,
+    });
+  },
+  goToSubListTab:function (e) {
+    var eFid = e.currentTarget.dataset.efid;
+    wx.navigateTo({
+      url: '../../pages/ejfl/ejfl?fid='+eFid,
     })
   },
   collectionTab:function() {
@@ -88,7 +94,6 @@ Page({
     var that = this;
     common.netWorkRequest({
       url: "xfenlei",
-      method: "POST",
       onSuccess: function (res) {
         var b = [];
         for (var i in res.data) {
