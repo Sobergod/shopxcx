ee42518c91ea0f4fc07359b847abb350dd1431a8
Lindanld <503982639@qq.com>
2018-03-30 16:18:31 +0800

    ld-pintuanliebiao

---
+++ pages/ptlb/ptlb.js
@@ -0,0 +1,71 @@
+// pages/ptlb/ptlb.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    collectionList: [
+      { photo: ['../../images/two.jpg'], name: ["玻尿酸晚安睡眠面膜aaaaaa"], money: ["￥99"], },
+      { photo: ['../../images/one_02.jpg'], name: ["玻尿酸晚安睡眠面膜"], money: ["￥99"], },
+      { photo: ['../../images/one_02.jpg'], name: ["玻尿酸晚安睡眠面膜"], money: ["￥99"], },
+      { photo: ['../../images/one_02.jpg'], name: ["玻尿酸晚安睡眠面膜"], money: ["￥99"], }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
